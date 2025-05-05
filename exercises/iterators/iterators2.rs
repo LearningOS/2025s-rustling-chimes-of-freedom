cce96a4b6a318194775a4c34aa2e63a49dbbadf1
chimes-of-freedom <167230294+chimes-of-freedom@users.noreply.github.com>
2025-05-05 04:51:39 +0000

    update

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,8 +6,6 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
@@ -28,7 +26,9 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words.iter()
+         .map(|word| capitalize_first(word))
+         .collect()
 }
 
 // Step 3.
@@ -36,7 +36,17 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let capitalized_words: Vec<String> =
+    words.iter()
+         .map(|word| capitalize_first(word))
+         .collect();
+
+    let mut result = String::new();
+    for word in capitalized_words {
+        result.push_str(&word);
+    }
+
+    result
 }
 
 #[cfg(test)]
